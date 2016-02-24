040e33ac82d081bb09f086ecdabef93ab2356f96
Shalini Kedlaya <skedlaya@gmail.com>
2016-02-24 08:24:57 +0000

    Aligned homepage to fit in a page

--- public/js/gruperScript.js
+++ public/js/gruperScript.js
@@ -34,7 +34,7 @@ function changeLoginData(result){
 	for(var i = 1; i < result.logindata.length; i++){
 		if(result.logindata[i].currentusr == "1"){
 			document.getElementById("username").innerHTML = result.logindata[i].username + " (Logout)";
-			$('#loginbuttons').html('<a href="/create"> <button id="newEvent" type="button" class="btn btn-info btn-large">Create New Event</button></a> <a href="/joined"> </br> </br> <button id="viewEvent" type="button" class="btn btn-info btn-large">Events You&#39ve Created/Joined</button></a>');
+			$('#loginbuttons').html('<a href="/create"> <button id="newEvent" type="button" class="btn btn-info btn-large">Create New Event</button></a> <a href="/joined">  <button id="viewEvent" type="button" class="btn btn-info btn-large">Events You&#39ve Joined</button></a>');
 			
 			resultFound = true;
 		}
