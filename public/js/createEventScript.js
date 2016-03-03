36c6ee698f78922a7f56ccb81d2412559a8e3302
bhaddad5 <bhaddad5@gmail.com>
2016-03-03 13:48:07 -0800

    loc fix

--- public/js/createEventScript.js
+++ public/js/createEventScript.js
@@ -6,6 +6,9 @@ $(document).ready(function() {
 	$(".info").toggle();
 });
 
+var input = /** @type {!HTMLInputElement} */(document.getElementById('location'));
+var autocomplete = new google.maps.places.Autocomplete(input);
+
 /*
  * Function that is called when the document is ready.
  */
@@ -16,6 +19,7 @@ function initializePage() {
 	document.getElementById("inviteDiv").style.display = 'none';
 	document.getElementById("hiddenlat").style.display = 'none';
 	document.getElementById("hiddenlng").style.display = 'none';
+	document.getElementById("locNotFound").style.display = 'none';
 	
 	$("#showDate2").unbind("click").click(function(){
         $("#date2").toggle();
@@ -34,6 +38,43 @@ function initializePage() {
 	setDefaultDate();
 }
 
+autocomplete.addListener('place_changed', function() {
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);  // Why 17? Because it looks good.
+    }
+    marker.setIcon(/** @type {google.maps.Icon} */({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+});
+
 function setDefaultDate(){
 	var today = new Date();
 	var dateString = (today.getMonth()+1) + "/" + today.getDate() + "/"+ today.getFullYear();
@@ -46,11 +87,12 @@ function getLatLong(){
 
 function latLongCallback(result){
 	if(result.results[0] == null && $("#hiddenlatlng").val() != ""){
-		alert("Google Maps Cannot Find: " + $("#location").val());
+		$("#locNotFound").show();
 		$("#hiddenlat").val("");
 		$("#hiddenlng").val("");
 	}
 	else{
+		$("#locNotFound").hide();
 		$("#hiddenlat").val(result.results[0].geometry.location.lat);
 		$("#hiddenlng").val(result.results[0].geometry.location.lng);
 	}
